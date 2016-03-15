a859b8cd19b8c60a28c32747fe757f9c5a06d88a
Geert Huls <geert.huls82@gmail.com>
2016-03-15 15:52:20 +0100

    Additional tests on structural equality

--- DddInPractice.Tests/MoneySpecs.cs
+++ DddInPractice.Tests/MoneySpecs.cs
@@ -24,5 +24,25 @@ namespace DddInPractice.Tests
             sum.FiveDollarCount.Should().Be(10);
             sum.TwentyDollarCount.Should().Be(12);
         }
+
+        [Fact]
+        public void Two_money_intances_equal_if_contain_the_same_money_amount()
+        {
+            var money1 = new Money(1, 2, 3, 4, 5, 6);
+            var money2 = new Money(1, 2, 3, 4, 5, 6);
+
+            money1.Should().Be(money2);
+            money1.GetHashCode().Should().Be(money2.GetHashCode());
+        }
+
+        [Fact]
+        public void Two_money_instances_differ_with_other_money_amount()
+        {
+            var dollar = new Money(0, 0, 0, 1, 0, 0);
+            var hunderedCents = new Money(100, 0, 0, 0, 0, 0);
+
+            dollar.Should().NotBe(hunderedCents);
+            dollar.GetHashCode().Should().NotBe(hunderedCents.GetHashCode());
+        }
     }
 }
