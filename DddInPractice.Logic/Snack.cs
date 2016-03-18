ee22e789fc14e3c86ef97cba18c55d3190f183c2
Geert Huls <geert.huls82@gmail.com>
2016-03-18 16:16:48 +0100

    Snackmachine with slots and snacks

---
+++ DddInPractice.Logic/Snack.cs
@@ -0,0 +1,17 @@
+﻿namespace DddInPractice.Logic
+{
+    public class Snack : Entity
+    {
+        public virtual string Name { get; protected set; }
+
+        protected Snack()
+        {
+            
+        }
+
+        public Snack(string name) : this()
+        {
+            Name = name;
+        }
+    }
+}
