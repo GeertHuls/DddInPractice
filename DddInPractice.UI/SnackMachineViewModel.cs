b2c7359c63e3e2f52c98bbc7f0bf67d0a120c833
Geert Huls <geert.huls82@gmail.com>
2016-03-18 12:00:36 +0100

    Wpf screen setup

---
+++ DddInPractice.UI/SnackMachineViewModel.cs
@@ -0,0 +1,16 @@
+﻿using DddInPractice.Logic;
+using DddInPractice.UI.Common;
+
+namespace DddInPractice.UI
+{
+    public class SnackMachineViewModel : ViewModel
+    {
+        private readonly SnackMachine _snackMachine;
+        public override string Caption => "Snack machine";
+
+        public SnackMachineViewModel(SnackMachine snackMachine)
+        {
+            _snackMachine = snackMachine;
+        }
+    }
+}
