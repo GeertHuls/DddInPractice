54c86e7336f006c7212d275720654e833c52b464
Geert Huls <geert.huls82@gmail.com>
2016-04-05 22:02:43 +0200

    Changed snack temporary test + enable NHibernate profiler

--- DddInPractice.Tests/TempDbTests.cs
+++ DddInPractice.Tests/TempDbTests.cs
@@ -1,31 +1,52 @@
-﻿using DddInPractice.Logic;
+﻿using System.IO;
+using DddInPractice.Logic;
+using log4net.Config;
 using NHibernate.Tool.hbm2ddl;
 using Xunit;
+using static DddInPractice.Logic.Money;
 
 namespace DddInPractice.Tests
 {
     public class TempDbTests
     {
-        private readonly string _connectionString= @"Data Source=.\dev;Initial Catalog=SnackMachineDb;Integrated Security=SSPI;";
+        private const string ConnectionString = @"Data Source=.\dev;Initial Catalog=SnackMachineDb;Integrated Security=SSPI;";
 
-        [Fact]
+        public TempDbTests()
+        {
+            InitNHibernatProfiler();
+            InitNHibernateSession();
+        }
+
+        private static void InitNHibernatProfiler()
+        {
+            var log4NetConfig = new FileInfo("./log4net.config");
+            XmlConfigurator.Configure(log4NetConfig);
+        }
+
+        private static void InitNHibernateSession()
+        {
+            SessionFactory.Init(ConnectionString);
+        }
+
+        [Fact(Skip = "Do not recreate database each time this test runs.")]
         public void CreateDatabase()
         {
-            SessionFactory.GetConfig(_connectionString)
+            SessionFactory.GetConfig(ConnectionString)
                 .ExposeConfiguration(c => new SchemaExport(c).Execute(true, true, false))
                 .BuildConfiguration();
         }
 
         [Fact(Skip = "Prevent unintentional queries to the database.")]
-        public void Test()
+        public void GetSnackMachineAndBuySnackTest()
         {
-            SessionFactory.Init(_connectionString);
-
-            using (var session = SessionFactory.OpenSession())
-            {
-                long id = 1;
-                var snackMachine = session.Get<SnackMachine>(id);
-            }
+            const long id = 1;
+            var repository = new SnackMachineRepository();
+            var snackMachine = repository.GetById(id);
+            snackMachine.InsertMoney(Dollar);
+            snackMachine.InsertMoney(Dollar);
+            snackMachine.InsertMoney(Dollar);
+            snackMachine.BuySnack(1);
+            repository.Save(snackMachine);
         }
     }
 }
